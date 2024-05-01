3b5224109d6a5298b45b913a22d6623db5855322
kenneth <kenneth@sensade.com>
2024-05-01 09:17:14 +0200

    added configuration file

--- database-load-test/emit_query_start.py
+++ database-load-test/emit_query_start.py
@@ -1,7 +1,17 @@
 import pika
+import configparser
+
+config = configparser.ConfigParser()
+
+config.read(r'src/config/settings/config.ini')
+
+mq_server_host = config.get("MQ", "mq_server_host")
+mq_user = config.get("MQ", "mq_user")
+mq_password = config.get("MQ", "mq_password")
+credentials = pika.PlainCredentials(mq_user, mq_password)
 
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+    pika.ConnectionParameters(host='localhost', credentials=credentials))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='query', exchange_type='fanout')
